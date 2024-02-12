cb1de6a270485d22f94853a85e35f279fd2df531
Hibari <jongxiankhoo@gmail.com>
2024-02-13 01:23:10 +0800

    feat: update video player props

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -1,14 +1,16 @@
 import dynamic from "next/dynamic";
 const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
 
-export const VideoPlayer = () => {
+export const VideoPlayer = (props: VideoProps) => {
+  const { className, width, height, videoUrl } = props;
+
   // Only loads the YouTube player
   return (
-    <div>
+    <div className={className}>
       <ReactPlayer
-        width={250}
-        height={250}
-        url="https://www.youtube.com/watch?v=LXb3EKWsInQ"
+       width="100%"
+       height="100%"
+        url={videoUrl || "https://www.youtube.com/watch?v=LXb3EKWsInQ"}
       />
     </div>
   );
---
+++ src/components/VideoPlayer/props.ts
@@ -0,0 +1,18 @@
+/** Video Player props */
+type VideoProps = {
+    /** @title main title */
+    title?: string;
+  
+    /** @width width*/
+    width?: string | number;
+  
+    /** @height height */
+    height?: string | number;
+  
+    /** @videoUrl video url  */
+    videoUrl?: string;
+  
+    /** @className overrides default classname */
+    className?: string;
+  };
+  
