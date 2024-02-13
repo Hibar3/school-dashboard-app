16f62ecde4bc450f153d71a907c5973d7a40474b
Hibari <jongxiankhoo@gmail.com>
2024-02-13 19:03:15 +0800

    fix: fix build issue due to images

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
+        domains: ['api-production.s3.amazonaws.com', 'i.pravatar.cc', 'flowbite.com' ,'cdn-icons-png.flaticon.com'], // whitelist custom domains
         remotePatterns: [
           {
             protocol: 'https',
--- src/containers/Comments/index.tsx
+++ src/containers/Comments/index.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export const Comments = () => {
   return (
     <section className="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
@@ -30,10 +32,12 @@ export const Comments = () => {
           <footer className="flex items-center justify-between mb-2">
             <div className="flex items-center">
               <p className="inline-flex items-center mr-3 font-semibold text-gray-900 text-sm dark:text-white">
-                <img
+                <Image
                   className="mr-2 rounded-full w-6 h-6"
                   src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
                   alt="Michael Gough"
+                  width={200}
+                  height={200}
                 />
                 Michael Gough
               </p>
@@ -130,10 +134,12 @@ export const Comments = () => {
           <footer className="flex items-center justify-between mb-2">
             <div className="flex items-center">
               <p className="inline-flex items-center mr-3 font-semibold text-gray-900 text-sm dark:text-white">
-                <img
+                <Image
                   className="mr-2 rounded-full w-6 h-6"
                   src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
                   alt="Jese Leos"
+                  width={200}
+                  height={200}
                 />
                 Jese Leos
               </p>
@@ -227,10 +233,12 @@ export const Comments = () => {
           <footer className="flex items-center justify-between mb-2">
             <div className="flex items-center">
               <p className="inline-flex items-center mr-3 font-semibold text-gray-900 text-sm dark:text-white">
-                <img
+                <Image
                   className="mr-2 rounded-full w-6 h-6"
                   src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
                   alt="Bonnie Green"
+                  width={200}
+                  height={200}
                 />
                 Bonnie Green
               </p>
@@ -296,103 +304,6 @@ export const Comments = () => {
           <p className="text-gray-500 dark:text-gray-400">
             The article covers the essentials, challenges, myths and stages the
             UX designer should consider while creating the design strategy.
-          </p>
-          <div className="flex items-center mt-4 space-x-4">
-            <button
-              type="button"
-              className="flex items-center font-medium text-gray-500 text-sm hover:underline dark:text-gray-400"
-            >
-              <svg
-                className="mr-1.5 w-3.5 h-3.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 20 18"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M5 5h5M5 8h2m6-3h2m-5 3h6m2-7H2a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1h3v5l5-5h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1Z"
-                />
-              </svg>
-              Reply
-            </button>
-          </div>
-        </article>
-        <article className="bg-white dark:bg-gray-900 p-6 border-t border-gray-200 dark:border-gray-700 text-base">
-          <footer className="flex items-center justify-between mb-2">
-            <div className="flex items-center">
-              <p className="inline-flex items-center mr-3 font-semibold text-gray-900 text-sm dark:text-white">
-                <img
-                  className="mr-2 rounded-full w-6 h-6"
-                  src="https://flowbite.com/docs/images/people/profile-picture-4.jpg"
-                  alt="Helene Engels"
-                />
-                Helene Engels
-              </p>
-              <p className="text-gray-600 text-sm dark:text-gray-400">
-                <time dateTime="2022-06-23" title="June 23rd, 2022">
-                  Jun. 23, 2022
-                </time>
-              </p>
-            </div>
-            <button
-              id="dropdownComment4Button"
-              data-dropdown-toggle="dropdownComment4"
-              className="inline-flex items-center bg-white hover:bg-gray-100 dark:hover:bg-gray-700 dark:bg-gray-900 p-2 rounded-lg font-medium text-center text-gray-500 text-sm dark:text-gray-40 focus:ring-4 focus:outline-none focus:ring-gray-50 dark:focus:ring-gray-600"
-              type="button"
-            >
-              <svg
-                className="w-4 h-4"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 16 3"
-              >
-                <path d="M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z" />
-              </svg>
-            </button>
-
-            <div
-              id="dropdownComment4"
-              className="z-10 bg-white dark:bg-gray-700 shadow w-36 divide-gray-100 divide-y hidden rounded dark:divide-gray-600"
-            >
-              <ul
-                className="py-1 text-gray-700 text-sm dark:text-gray-200"
-                aria-labelledby="dropdownMenuIconHorizontalButton"
-              >
-                <li>
-                  <a
-                    href="#"
-                    className="block hover:bg-gray-100 dark:hover:bg-gray-600 px-4 py-2 dark:hover:text-white"
-                  >
-                    Edit
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block hover:bg-gray-100 dark:hover:bg-gray-600 px-4 py-2 dark:hover:text-white"
-                  >
-                    Remove
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="block hover:bg-gray-100 dark:hover:bg-gray-600 px-4 py-2 dark:hover:text-white"
-                  >
-                    Report
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </footer>
-          <p className="text-gray-500 dark:text-gray-400">
-            Thanks for sharing this. I do came from the Backend development and
-            explored some of the tools to design my Side Projects.
           </p>
           <div className="flex items-center mt-4 space-x-4">
             <button
