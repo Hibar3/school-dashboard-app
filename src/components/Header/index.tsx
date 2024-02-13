fdd0606b5863a57a5eea4ef0b7059e4691f1d95c
Hibari <jongxiankhoo@gmail.com>
2024-02-14 00:03:00 +0800

    feat: add context theme switcher

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,25 @@
-type Props = {
+import { ThemeSwitcher } from "@/app/lib/ThemeSwitcher";
+import Image from "next/image";
+
+/** Header Props */
+type HeaderProps = {
   title?: string;
   isToggled?: boolean;
   onToggle?: () => void;
 };
 
-export const Header = (props?: Props) => {
+export const Header = (props?: HeaderProps) => {
   const { title, isToggled, onToggle } = props || {};
 
   return (
     <header className="max-w-full">
       <nav className="bg-gray-800">
-        <div className="max-w-full px-2 sm:px-4 lg:px-2">
-          <div className="flex h-16 items-center justify-between">
-            <div className="inset-y-0 left-0 flex items-center">
+        <div className="px-2 sm:px-4 lg:px-2 max-w-full">
+          <div className="flex items-center justify-between h-16">
+            <div className="left-0 inset-y-0 flex items-center">
               <button
                 type="button"
-                className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                className="relative inline-flex items-center justify-center hover:bg-gray-700 p-2 rounded-md text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                 aria-controls="mobile-menu"
                 aria-expanded="false"
                 onClick={onToggle}
@@ -23,7 +27,7 @@ export const Header = (props?: Props) => {
                 <span className="absolute -inset-0.5"></span>
                 <span className="sr-only">Open main menu</span>
                 <svg
-                  className="block h-6 w-6"
+                  className="block w-6 h-6"
                   fill="none"
                   viewBox="0 0 24 24"
                   strokeWidth="1.5"
@@ -38,7 +42,7 @@ export const Header = (props?: Props) => {
                 </svg>
 
                 <svg
-                  className="hidden h-6 w-6"
+                  className="w-6 h-6 hidden"
                   fill="none"
                   viewBox="0 0 24 24"
                   strokeWidth="1.5"
@@ -53,34 +57,28 @@ export const Header = (props?: Props) => {
                 </svg>
               </button>
             </div>
-            <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-              <div className="hidden sm:ml-6 sm:block">
+            <div className="flex flex-1 items-center sm:items-stretch justify-center sm:justify-start">
+              <div className="sm:block sm:ml-6 hidden">
                 <div className="flex space-x-4">
                   <a
                     href="#"
-                    className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-                    aria-current="page"
+                    className="hover:bg-gray-700 px-3 py-2 rounded-md font-medium text-gray-300 text-sm hover:text-white"
                   >
-                    Dashboard
-                  </a>
-                  <a
-                    href="#"
-                    className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                  >
-                    {title}
+                    {title || "Dashboard"}
                   </a>
                 </div>
               </div>
             </div>
-            <div className="inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <div className="right-0 sm:static inset-y-0 sm:inset-auto flex items-center sm:ml-6 pr-2 sm:pr-0">
+              <ThemeSwitcher />
               <button
                 type="button"
-                className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                className="relative bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
               >
                 <span className="absolute -inset-1.5"></span>
                 <span className="sr-only">View notifications</span>
                 <svg
-                  className="h-6 w-6"
+                  className="w-6 h-6"
                   fill="none"
                   viewBox="0 0 24 24"
                   strokeWidth="1.5"
@@ -94,27 +92,28 @@ export const Header = (props?: Props) => {
                   />
                 </svg>
               </button>
-
               <div className="relative ml-3">
                 <div>
                   <button
                     type="button"
-                    className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    className="relative flex bg-gray-800 rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
                     id="user-menu-button"
                     aria-expanded="false"
                     aria-haspopup="true"
                   >
                     <span className="absolute -inset-1.5"></span>
                     <span className="sr-only">Open user menu</span>
-                    <img
-                      className="h-8 w-8 rounded-full"
+                    <Image
+                      className="rounded-full w-8 h-8"
                       src="https://cdn-icons-png.flaticon.com/512/4646/4646084.png"
-                      alt=""
+                      width={200}
+                      height={200}
+                      alt="/"
                     />
                   </button>
                 </div>
                 {/* <div
-                  className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+                  className="right-0 z-10 absolute bg-white ring-opacity-5 shadow-lg mt-2 py-1 rounded-md w-48 origin-top-right ring-1 ring-black focus:outline-none"
                   role="menu"
                   aria-orientation="vertical"
                   aria-labelledby="user-menu-button"
@@ -122,7 +121,7 @@ export const Header = (props?: Props) => {
                 >
                   <a
                     href="#"
-                    className="block px-4 py-2 text-sm text-gray-700"
+                    className="block px-4 py-2 text-gray-700 text-sm"
                     role="menuitem"
                     tabIndex={-1}
                     id="user-menu-item-0"
@@ -131,7 +130,7 @@ export const Header = (props?: Props) => {
                   </a>
                   <a
                     href="#"
-                    className="block px-4 py-2 text-sm text-gray-700"
+                    className="block px-4 py-2 text-gray-700 text-sm"
                     role="menuitem"
                     tabIndex={-1}
                     id="user-menu-item-1"
@@ -140,7 +139,7 @@ export const Header = (props?: Props) => {
                   </a>
                   <a
                     href="#"
-                    className="block px-4 py-2 text-sm text-gray-700"
+                    className="block px-4 py-2 text-gray-700 text-sm"
                     role="menuitem"
                     tabIndex={-1}
                     id="user-menu-item-2"
@@ -154,17 +153,17 @@ export const Header = (props?: Props) => {
         </div>
         {/* Mobile View */}
         {/* <div className="sm:hidden" id="mobile-menu">
-          <div className="space-y-1 px-2 pb-3 pt-2">
+          <div className="px-2 pt-2 pb-3 space-y-1">
             <a
               href="#"
-              className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
+              className="block bg-gray-900 px-3 py-2 rounded-md font-medium text-base text-white"
               aria-current="page"
             >
               Dashboard
             </a>
             <a
               href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
+              className="block hover:bg-gray-700 px-3 py-2 rounded-md font-medium text-base text-gray-300 hover:text-white"
             >
               Calendar
             </a>
