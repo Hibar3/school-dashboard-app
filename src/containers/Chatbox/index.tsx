a1778bbbdc02212bb2b46468c789bb7c9ea5dd9b
Hibari <jongxiankhoo@gmail.com>
2024-02-15 02:24:49 +0800

    style: adjust chat box dark mode styles

--- src/containers/Chatbox/index.tsx
+++ src/containers/Chatbox/index.tsx
@@ -18,7 +18,7 @@ export const Chatbox = () => {
           </div>
         </div>
 
-        <div className="flex-1 bg-white dark:bg-slate-900 p-2 overflow-y-auto">
+        <div className="flex-1 bg-white dark:bg-gray-500 p-2 overflow-y-auto">
           {/* Chat LTR */}
           <div className="flex items-start gap-2.5 mb-4">
             <Image
@@ -28,7 +28,7 @@ export const Chatbox = () => {
               height={200}
               alt="placeholder"
             />
-            <div className="flex flex-col bg-gray-700 dark:bg-gray-700 p-4 border-gray-200 rounded-e-xl rounded-es-xl w-full max-w-44 leading-1.5">
+            <div className="flex flex-col bg-gray-700 dark:bg-gray-700 shadow p-4 rounded-e-xl rounded-es-xl w-full max-w-44 leading-1.5">
               <div className="flex items-center space-x-2 rtl:space-x-reverse">
                 <span className="font-semibold text-xs ">Bonnie Green</span>
                 <span className="font-normal text-gray-400 text-xs dark:text-gray-500">
@@ -47,7 +47,7 @@ export const Chatbox = () => {
 
           {/* Chat RTL */}
           <div className="flex flex-row-reverse mb-4">
-            <div className="flex flex-col bg-gray-100 dark:bg-gray-700 p-2.5 border-gray-200 rounded-ee-xl rounded-s-xl w-full max-w-[320px] leading-1.5">
+            <div className="flex flex-col bg-gray-100 dark:bg-gray-700 shadow p-2.5 border-gray-200 rounded-ee-xl rounded-s-xl w-full max-w-[320px] leading-1.5">
               <div className="flex items-center space-x-2 rtl:space-x-reverse">
                 <span className="font-semibold text-gray-900 text-xs dark:text-white">
                   You
@@ -67,16 +67,16 @@ export const Chatbox = () => {
           </div>
         </div>
 
-        <div className="flex items-center p-2 border-t">
+        <div className="flex items-center p-2">
           <div className="mx-2 w-full">
             <input
-              className="bg-slate-600 border border-gray-200 rounded-full sm:w-full"
+              className="border border-gray-200 rounded-full w-1/2 sm:w-full"
               type="text"
               defaultValue=""
               placeholder="Aa"
             />
           </div>
-          <div>
+          <div className="">
             <button
               className="inline-flex hover:bg-indigo-50 p-2 rounded-full"
               type="button"
