966ceec2656d248ea4ff9c9e44a166a39baaef44
Hibari <jongxiankhoo@gmail.com>
2024-04-13 18:56:20 +0800

    feat: add profile card component

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,12 +1,17 @@
 import { Card, Dropdown, DropdownItem } from "flowbite-react";
 import Image from "next/image";
 
+type ProfileProps = {
+  className?: string;
+  enableButtons?: boolean;
+};
 
-export const Profile = (props?: any) => {
+export const Profile = (props?: ProfileProps) => {
+  const { className, enableButtons = false } = props || {};
   return (
-    <Card className="max-w-sm">
-      <div className="flex justify-end px-4 pt-4">
-        <Dropdown inline label="">
+    <Card className="sm:max-w-sm">
+      <div className="flex justify-end">
+        <Dropdown inline label="" className="ml-2">
           <DropdownItem>
             <a
               href="#"
@@ -36,9 +41,9 @@ export const Profile = (props?: any) => {
       <div className="flex flex-col items-center pb-10">
         <Image
           alt="Bonnie image"
-          height="96"
+          height="80"
           src="https://cdn-icons-png.flaticon.com/512/4646/4646084.png"
-          width="96"
+          width="80"
           className="shadow-lg mb-3 rounded-full"
         />
         <h5 className="mb-1 font-medium text-gray-900 text-xl dark:text-white">
@@ -47,20 +52,22 @@ export const Profile = (props?: any) => {
         <span className="text-gray-500 text-sm dark:text-gray-400">
           Visual Designer
         </span>
-        <div className="flex mt-4 lg:mt-6 space-x-3">
-          <a
-            href="#"
-            className="inline-flex items-center bg-cyan-700 hover:bg-cyan-800 dark:hover:bg-cyan-700 dark:bg-cyan-600 px-4 py-2 rounded-lg font-medium text-center text-sm text-white focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:focus:ring-cyan-800"
-          >
-            Add friend
-          </a>
-          <a
-            href="#"
-            className="inline-flex items-center bg-white hover:bg-gray-100 dark:hover:bg-gray-700 dark:bg-gray-800 px-4 py-2 border border-gray-300 dark:hover:border-gray-700 dark:border-gray-600 rounded-lg font-medium text-center text-gray-900 text-sm focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-white dark:focus:ring-gray-700"
-          >
-            Message
-          </a>
-        </div>
+        {enableButtons && (
+          <div className="flex space-x-3 mt-4 lg:mt-6">
+            <a
+              href="#"
+              className="inline-flex items-center bg-cyan-700 hover:bg-cyan-800 dark:hover:bg-cyan-700 dark:bg-cyan-600 px-4 py-2 rounded-lg font-medium text-center text-sm text-white focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:focus:ring-cyan-800"
+            >
+              Add friend
+            </a>
+            <a
+              href="#"
+              className="inline-flex items-center border-gray-300 dark:hover:border-gray-700 dark:border-gray-600 bg-white hover:bg-gray-100 dark:hover:bg-gray-700 dark:bg-gray-800 px-4 py-2 border rounded-lg font-medium text-center text-gray-900 text-sm focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-white dark:focus:ring-gray-700"
+            >
+              Message
+            </a>
+          </div>
+        )}
       </div>
     </Card>
   );
