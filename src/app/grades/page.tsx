bf83aed553b8403f1b0cfd1d701337f076419199
Hibari <jongxiankhoo@gmail.com>
2024-04-14 16:37:21 +0800

    feat: add charts

--- src/app/grades/page.tsx
+++ src/app/grades/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { Header, Profile, SideBar, VideoPlayer } from "@/components";
+import { Profile } from "@/components";
 import View from "@/containers/View";
-import Chatbox from "@/containers/Chatbox";
-import Comments from "@/containers/Comments";
 import {
   IoArrowForwardCircleOutline,
   IoBookOutline,
@@ -13,6 +11,24 @@ import {
 import { HiOutlineClipboardList } from "react-icons/hi";
 import GradeTable from "@/containers/Table";
 import { Pagination } from "flowbite-react";
+import Chart from "react-apexcharts";
+
+
+const options = {
+  chart: {
+    id: "apexchart-example",
+  },
+  xaxis: {
+    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+  },
+};
+
+const series = [
+  {
+    name: "series-1",
+    data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+  },
+];
 
 export default function Index() {
   const [currentPage, setCurrentPage] = useState(1);
@@ -22,14 +38,29 @@ export default function Index() {
   const toggleSidebar = () => {
     setIsOpen(!isOpen);
   };
+
+  const renderIcon = () => (
+    <svg
+      className="w-6 h-6 text-gray-500 dark:text-gray-400"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 20 19"
+    >
+      <path d="M14.5 0A3.987 3.987 0 0 0 11 2.1a4.977 4.977 0 0 1 3.9 5.858A3.989 3.989 0 0 0 14.5 0ZM9 13h2a4 4 0 0 1 4 4v2H5v-2a4 4 0 0 1 4-4Z" />
+      <path d="M5 19h10v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2ZM5 7a5.008 5.008 0 0 1 4-4.9 3.988 3.988 0 1 0-3.9 5.859A4.974 4.974 0 0 1 5 7Zm5 3a3 3 0 1 0 0-6 3 3 0 0 0 0 6Zm5-1h-.424a5.016 5.016 0 0 1-1.942 2.232A6.007 6.007 0 0 1 17 17h2a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5ZM5.424 9H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h2a6.007 6.007 0 0 1 4.366-5.768A5.016 5.016 0 0 1 5.424 9Z" />
+    </svg>
+  );
+
   return (
     <View>
       <div>
         <div className="border-gray-700 p-1 rounded-lg">
           {/* Double panel view -  Left + Right  */}
-          <div className="gap-1 sm:grid grid-cols-2 grid-flow-col mb-4">
+          <div className="gap-1 sm:grid grid-cols-2 grid-rows-2 grid-flow-col mb-4">
             {/* Left Panel */}
             <div className="col-span-2 row-span-2 p-2 sm:w-full max-w-sm sm:max-w-screen-2xl">
+              {/* Table */}
               <div className="p-2 border border-dashed rounded-lg">
                 <p className="font-bold text-black dark:text-white">Grades</p>
                 <GradeTable />
@@ -44,14 +75,77 @@ export default function Index() {
               {/* End of Table */}
 
               {/* Chart Section*/}
-              <div className="mt-2 p-2 border border-dashed rounded-lg">
-                <div className="gap-4 grid grid-flow-col grid-col-2">
-                  <div className="mt-2 p-2 border border-dashed rounded-lg">
-                    01
+              <div className="sm:flex sm:flex-1 mt-2 p-1 border border-dashed rounded-lg w-full overflow-x-auto">
+                {/* Bar Chart */}
+                <div className="sm:flex sm:flex-1 m-2 border border-dashed rounded-lg w-full">
+                  <div className="sm:flex sm:flex-1 bg-white dark:bg-gray-800 shadow p-2 sm:p-4 rounded-lg w-full">
+                    <div className="sm:flex justify-between border-gray-200 dark:border-gray-700 mb-4 pb-4 border-b">
+                      <div className="flex border border-dashed">
+                        <div className="flex justify-center items-center bg-gray-100 dark:bg-gray-700 rounded-lg w-12 h-12 me-3">
+                          {renderIcon()}
+                        </div>
+                        <div>
+                          <h5 className="pb-1 font-bold text-gray-900 text-xl dark:text-white leading-none">
+                            In Perspective
+                          </h5>
+                          <p className="font-normal text-gray-500 text-sm dark:text-gray-400">
+                            Scores by subjects
+                          </p>
+                          <div
+                            id="bar-chart"
+                            className="border border-dashed overflow-x-auto"
+                          >
+                            <Chart
+                              options={options}
+                              series={series}
+                              type="bar"
+                              width={"280"}
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <span className="inline-flex items-center bg-green-100 dark:bg-green-900 px-2.5 py-1 rounded-md font-medium text-green-800 text-xs dark:text-green-300">
+                            Active
+                          </span>
+                        </div>
+                      </div>
+                    </div>
                   </div>
-
-                  <div className="mt-2 p-2 border border-dashed rounded-lg">
-                    09
+                </div>
+                {/* Pie Chart */}
+                <div className="sm:flex sm:flex-1 m-2 border border-dashed rounded-lg w-full">
+                  <div className="sm:flex sm:flex-1 bg-white dark:bg-gray-800 shadow p-4 sm:p-6 rounded-lg w-full">
+                    <div className="sm:flex justify-between border-gray-200 dark:border-gray-700 mb-4 pb-4 border-b">
+                      <div className="flex border border-dashed">
+                        <div className="flex justify-center items-center bg-gray-100 dark:bg-gray-700 rounded-lg w-12 h-12 me-3">
+                          {renderIcon()}
+                        </div>
+                        <div>
+                          <h5 className="pb-1 font-bold text-gray-900 text-xl dark:text-white leading-none">
+                            In Perspective
+                          </h5>
+                          <p className="font-normal text-gray-500 text-sm dark:text-gray-400">
+                            Scores by subjects
+                          </p>
+                          <div
+                            id="bar-chart"
+                            className="border border-dashed overflow-x-auto"
+                          >
+                            <Chart
+                              options={options}
+                              series={series}
+                              type="bar"
+                              width={"280"}
+                            />
+                          </div>
+                        </div>
+                        <div>
+                          <span className="sm:inline-flex items-center bg-green-100 dark:bg-green-900 px-2.5 py-1 rounded-md font-medium text-green-800 text-xs dark:text-green-300">
+                            Active
+                          </span>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -60,9 +154,9 @@ export default function Index() {
 
             {/* Right Panel */}
             <div
-              className={`col-span-3 row-span-3 p-2 border-2 border-dashed border-gray-700 rounded-lg max-w-[300px] sm:${
+              className={`sm:col-span-3 sm:row-span-3 p-2 border-2 border-dashed border-gray-700 rounded-lg sm:max-w-[300px] sm:${
                 isOpen ? "w-48" : "w-64"
-              } transition-all duration-300`}
+              } max-w-sm w-10/12 min-w-48 transition-all duration-300`}
             >
               {/* Profile card */}
               <p className="font-bold text-black dark:text-white">Profile</p>
