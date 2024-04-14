5fd196bc7317495c6de9e3c85688cd1b4ed260af
Hibari <jongxiankhoo@gmail.com>
2024-04-14 19:55:18 +0800

    fix: window helper useEffect build error

--- src/app/lib/helper.ts
+++ src/app/lib/helper.ts
@@ -3,9 +3,10 @@ import { useEffect, useState } from "react";
 export const useSize = () => {
   let wSize: number[] = [0, 0];
   const [windowSize, setWindowSize] = useState([1440, 900]);
-  if (typeof window !== "undefined") {
-    // browser code
-    useEffect(() => {
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      // browser code
       const windowSizeHandler = () => {
         setWindowSize([window?.innerWidth, window?.innerHeight]);
       };
@@ -14,19 +15,8 @@ export const useSize = () => {
       return () => {
         window.removeEventListener("resize", windowSizeHandler);
       };
-    }, []);
-  } else {
-    useEffect(() => {
-      const windowSizeHandler = () => {
-        setWindowSize([window?.innerWidth, window?.innerHeight]);
-      };
-      window.addEventListener("resize", windowSizeHandler);
-
-      return () => {
-        window.removeEventListener("resize", windowSizeHandler);
-      };
-    }, []);
-  }
+    }
+  }, []);
 
   return windowSize;
 };
